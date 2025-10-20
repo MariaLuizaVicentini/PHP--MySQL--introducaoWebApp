4f34f877bd8d97aa13e5aa66f41ea339379e36f3
Malu Vicentini <vicentinimalu1@gmail.com>
2025-10-20 00:37:56 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php 
+    include 'Artigo.php'; // caminho do servidor
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Meu Blog</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div id="container">
+        <h1>Meu Blog</h1>
+
+        <?php foreach($artigos as $artigo): ?>
+
+            <h2>
+                <a href="<?= $artigo['link']; ?>">
+                    <?= $artigo['titulo']; ?>
+                </a>
+            </h2>
+            <p>
+                <?= $artigo['conteudo']; ?>
+            </p>
+        <?php endforeach; ?>
+        
+    </div>
+</body>
+
+</html>
